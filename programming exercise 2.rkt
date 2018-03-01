88c42d603272bb8efb96d87c41c2da1ab7bf2337
gdc24 <gdc24@case.edu>
2018-02-28 22:16:00 -0500

    done with up to num 9

--- programming exercise 2.rkt
+++ programming exercise 2.rkt
@@ -13,7 +13,7 @@
 ; insert takes a number and a list of numbers in order and inserts the number in the proper place.
 ; > (insert 7 '(1 4 5 6 9 10))
 ; (1 4 5 6 7 9 10)
-; works but sometimes uses 2 stack frames
+; done!
 
 (define insert
   (lambda (x lis)
@@ -23,13 +23,13 @@
   (lambda (x lis return)
     (cond
       ((null? lis) (return (cons x '())))
-      ((<= x (car lis)) (return (cons x lis)))                                     ; to do the comparison, it needs another stack frame
-      (else (insert-cps x (cdr lis) (lambda (v) (return (cons (car lis) v))))))))  ; to do the cons, it adds another stack frame
+      ((<= x (car lis)) (return (cons x lis)))
+      (else (insert-cps x (cdr lis) (lambda (v) (return (cons (car lis) v))))))))
 
 ; merge takes two lists of numbers that are in order and returns a list that contains the combination of both lists in order.
 ; > (merge '(3 5 6 7 9) '(0 1 2 4 6 8 9 10))
 ; (0 1 2 3 4 5 6 6 7 8 9 9 10)
-; works but sometimes uses two stack frames (i think...test again to see where)
+; done!
 
 (define merge
   (lambda (lis1 lis2)
@@ -46,15 +46,11 @@
 ; removedups takes a list of atoms and removes any atom that is a repeat of the atom that immediately precedes it.
 ; > (removedups '(a a b b b c c a b b))
 ; (a b c a b)
-; works but haven't tested to see how many stack frames it uses yet
+; done!
 
 (define removedups
   (lambda (lis)
-    (cond
-      ((null? lis) '())
-      ((null? (cdr lis)) (cons (car lis) '()))
-      ((eq? (car lis) (car (cdr lis))) (removedups (cons (car lis) (cdr (cdr lis)))))
-      (else (cons (car lis) (removedups (cdr lis)))))))
+    (removedups-cps lis (lambda (v) v))))
 
 (define removedups-cps
   (lambda (lis return)
@@ -69,14 +65,11 @@
 ; 1
 ; > (numparens '(1 () (()) (2 3 (4)))
 ; 6
-; works but sometimes uses 2 or 3 stack frames
+; done!
 
 (define numparens
   (lambda (lis)
-    (cond
-      ((null? lis) '1)
-      ((list? (car lis)) (+ (numparens (car lis)) (numparens (cdr lis))))
-      (else (numparens (cdr lis))))))
+    (numparens-cps lis (lambda (v) v))))
 
 (define numparens-cps
   (lambda (lis return)
@@ -95,16 +88,15 @@
   (lambda (lis)
     (cond
       ((null? lis) '())
-      ((pair? (car lis)) (cons (dup* (car lis)) (dup* (cdr lis))))
+      ((pair? (car lis)) (cons (dup* (car lis)) (cons (dup* (car lis)) (dup* (cdr lis)))))
       (else (cons (car lis) (cons (car lis) (dup* (cdr lis))))))))
 
 (define dup*-cps
   (lambda (lis return)
     (cond
       ((null? lis) (return '()))
-      ((pair? (car lis)) (dup*-cps (cdr lis) (lambda (v) (return (cons (dup*-cps (car lis) (lambda (v) v)) v)))))
+      ((pair? (car lis)) (dup*-cps (car lis) (lambda (v) (return (cons v (dup*-cps (cdr lis) (lambda (v) (cons (dup*-cps (car lis) (lambda (v) v)) v))))))))
       (else (dup*-cps (cdr lis) (lambda (v) (return (cons (car lis) (cons (car lis) v)))))))))
-
 
 ; removedups* takes a list, that can contain sublists, and removes any atom that is the repeat of the atom that immediately precedes it in the same sublist.
 ; > (removedups* '(a a (b b b (d d) b ((d) d)) f (f f g)))
@@ -134,42 +126,28 @@
 ; and then you call merge on the two lists returned by the recursive calls to mergesort.
 ; (mergesort '()) ==> '()
 ; (mergesort '(8 1 3 9 6 5 7 2 4 10)) ==> '(1 2 3 4 5 6 7 8 9 10)
-; not done
+; done but not done
 
+(define mymergesort
+  (lambda (lis)
+    (mergesort-cps lis (lambda (v) v))))
 
-
-
-; merge
-(define merge
-  (lambda (lis1 lis2)
+(define mergesort-cps
+  (lambda (lis return)
     (cond
-      ((null? lis1) lis2)
-      ((null? lis2) lis1)
-      ((<= (car lis1) (car lis2)) (cons (car lis1) (merge (cdr lis1) lis2)))
-      (else (cons (car lis2) (merge lis1 (cdr lis2)))))))
-
-; does not work
-(define merge-cps
-  (lambda (lis1 lis2 return)
-    (cond
-      ((null? lis1) (return lis2))
-      ((null? lis2) (return lis1))
-      ((<= (car lis1) (car lis2)) (merge-cps (cdr lis1) lis2 (lambda (v) (return (cons (car lis1) v)))))
-      (else (merge-cps lis1 (cdr lis2) (lambda (v) (return (cons (car lis2) v))))))))
+      ((null? lis) (return '()))
+      ((null? (cdr lis)) (return lis))
+      (else (mergesort-cps (cadr (split-cps lis (lambda (v1 v2) (list v1 v2)))) (lambda (v) (return (merge-cps (mergesort-cps (car (split-cps lis (lambda (v1 v2) (list v1 v2)))) (lambda (v) v)) v (lambda (v) v)))))))))
 
 
-
-      
-
-
-
-; the cps version of split.  call with the initial continution; (lambda (v1 v2) (list v1 v2))
+; the cps version of split
 (define split-cps
   (lambda (lis return)
     (cond
       ((null? lis) (return '() '()))
       ((null? (cdr lis)) (return lis '()))
       (else (split-cps (cddr lis) (lambda (v1 v2) (return (cons (car lis) v1) (cons (cadr lis) v2))))))))
+
 
 
 ; replaceatoms takes two lists. The first list can contain sublists, but the second list is a single list of atoms.
@@ -181,7 +159,10 @@
 ; ((z ((y) x w) ((((v) u g) (h i)) j (k l))) m n (o p))
 ; not done
 
-
+(define replaceatoms
+  (lambda (lis1 lis2)
+    ((null? lis1) lis1)
+    ()))
 
 
 
